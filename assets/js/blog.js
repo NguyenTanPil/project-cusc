2f2a010073a17f2e8215da27c2d7930cdc03a9b2
NguyenTanPil <traila1008@gmail.com>
2020-12-28 20:43:34 +0700

    Edit service.html and Add blog.html

---
+++ assets/js/blog.js
@@ -0,0 +1,78 @@
+Vue.component('new-blog', {
+	props: ['obj'],
+	template: `
+    <div class="row article-1 pb-5 mb-5">
+      <div class="col-12 position-relative px-0 py-0 not-over">
+        <a href="#">
+          <i class="fa fa-search position-absolute article-1-search d-none" aria-hidden="true"></i>
+          <img :src="obj.img" class="img-fluid" alt="Responsive image">
+        </a>
+        <div class="date d-flex flex-column font-weight-bold text-white align-items-center position-absolute">
+          <span class="month">{{obj.month}}</span>
+          <span class="day">{{obj.day}}</span>
+          <span class="year">{{obj.year}}</span>
+        </div>
+      </div>
+
+      <div class="col-12 pt-5">
+        <h3>
+          <a href="#" class="content-link">{{obj.title}}</a>
+        </h3>
+        <p class="text-muted mb-4" style="font-size: 90%;">Category: 
+          <a href="#" class="category-link">{{obj.c1}}</a>
+          <a href="#" class="category-link">{{obj.c2}}</a>
+        </p>
+        <p class="text-muted" style="font-size: 90%;">
+            {{obj.content}}
+        </p>
+        <div class="bt-article-1 d-flex justify-content-between">
+          <p class="py-1 mb-0">Posted by <span>admin</span></p>
+          <a href="services.html" class="btn btn-outline-info" role="button" aria-pressed="true">Read more</a>
+        </div>
+      </div>
+    </div>
+	`
+})
+
+// Khoi tao doi tuong vue
+var vue = new Vue({
+  el: '#at',
+  data: {
+    listBlog1:[
+    {
+      id: 1,
+      img: 'assets/images/article-1.jpg',
+      day: '15',
+      month: 'July',
+      year: '2020',
+      title: 'What is BGA',
+      c1: 'MICRO CHIP REPAIR,',
+      c2: 'TECHNOLOGY',
+      content: 'A ball grid array (BGA) is a type of surface-mount packaging (a chip carrier) used for integrated circuits. BGA packages are used to permanently mount devices such as microprocessors. A BGA can provide more interc... pins than can be put on a dual in-line or flat package. The whole bottom surface of the device can be used, instead of just the perimeter. The leads are also on average shorter than with a perimeter-only type, leading to better performance at high speeds. Soldering of BGA devices requires precise control and is usually done by automated processes. BGA devices are not suitable for socket mounting, normal distribution of letters, as opposed to using, Content here, content here, making it look like readable English.'
+    },
+    {
+      id: 2,
+      img: 'assets/images/article-2.jpg',
+      day: '16',
+      month: 'July',
+      year: '2020',
+      title: 'Calibrating the Home button in iPhone, iPad and iPod touch',
+      c1: ' IPHONE,',
+      c2: 'TECHNOLOGY',
+      content: 'A ball grid array (BGA) is a type of surface-mount packaging (a chip carrier) used for integrated circuits. BGA packages are used to permanently mount devices such as microprocessors. A BGA can provide more interc... pins than can be put on a dual in-line or flat package. The whole bottom surface of the device can be used, instead of just the perimeter. The leads are also on average shorter than with a perimeter-only type, leading to better performance at high speeds. Soldering of BGA devices requires precise control and is usually done by automated processes. BGA devices are not suitable for socket mounting, normal distribution of letters, as opposed to using, Content here, content here, making it look like readable English.'
+    },
+    {
+      id: 3,
+      img: 'assets/images/article-3.jpg',
+      day: '17',
+      month: 'July',
+      year: '2020',
+      title: 'What is BGA',
+      c1: 'BGA, THECHNOLOGY,',
+      c2: 'REPLACE',
+      content: 'A ball grid array (BGA) is a type of surface-mount packaging (a chip carrier) used for integrated circuits. BGA packages are used to permanently mount devices such as microprocessors. A BGA can provide more interc... pins than can be put on a dual in-line or flat package. The whole bottom surface of the device can be used, instead of just the perimeter. The leads are also on average shorter than with a perimeter-only type, leading to better performance at high speeds. Soldering of BGA devices requires precise control and is usually done by automated processes. BGA devices are not suitable for socket mounting, normal distribution of letters, as opposed to using, Content here, content here, making it look like readable English.'
+    },
+    
+    ]
+  }
+});
---
+++ assets/js/services.js
@@ -0,0 +1,422 @@
+Vue.component('new-product', {
+	props: ['obj'],
+	template: `
+    <div class="card my-card-product mx-sm-auto" >
+      <a href="#"><img class="card-img-top my-img-product" :src="obj.img" alt="Card image cap"></a>
+      <div class="card-body">
+        <h6 class="card-title"><a href="#">{{obj.title}}</a></h6>
+        <p class="card-text text-muted">{{obj.content}}</p>
+        <div class="read-more-product d-flex justify-content-end">
+          <a href="#" class="btn btn-outline-info" role="button" aria-pressed="true">Read more</a>
+        </div>
+      </div>
+    </div>
+	`
+})
+
+// Khoi tao doi tuong vue
+var vue = new Vue({
+	el: '.app',
+	data: {
+		listNew1: [
+		{
+			id: 1,
+			img: 'assets/images/mac-4.png',
+			title: 'Laptop Apple MacBook Pro 2018',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 2,
+			img: 'assets/images/dell-2.png',
+			title: 'Laptop Dell Inspiron 7373-TI501OW',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 3,
+			img: 'assets/images/asus-3.png',
+			title: 'Laptop ASUS ZenBook UX430UA',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 4,
+			img: 'assets/images/acer-2.png',
+			title: 'Laptop Acer Swift 3 SF314',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 5,
+			img: 'assets/images/mac-3.png',
+			title: 'MacBook Air 2017 128GB (MQD32SA/A)',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 6,
+			img: 'assets/images/asus-2.png',
+			title: 'Laptop Asus Vivobook S533EQ',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 7,
+			img: 'assets/images/mac-4.png',
+			title: 'Laptop Apple MacBook Pro 2018',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 8,
+			img: 'assets/images/dell-2.png',
+			title: 'Laptop Dell Inspiron 7373-TI501OW',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 9,
+			img: 'assets/images/asus-3.png',
+			title: 'Laptop ASUS ZenBook UX430UA',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 10,
+			img: 'assets/images/acer-2.png',
+			title: 'Laptop Acer Swift 3 SF314',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 11,
+			img: 'assets/images/mac-3.png',
+			title: 'MacBook Air 2017 128GB (MQD32SA/A)',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 12,
+			img: 'assets/images/asus-2.png',
+			title: 'Laptop Asus Vivobook S533EQ',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		]
+	}
+});
+
+
+// Khoi tao doi tuong vue
+var vue = new Vue({
+	el: '.app2',
+	data: {
+		listNew2: [
+		{
+			id: 1,
+			img: 'assets/images/mac-1.png',
+			title: 'MacBook Air 2017 128GB (MQD32SA/A)',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 2,
+			img: 'assets/images/mac-2.png',
+			title: 'MacBook Air M1 256GB Yellow',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 3,
+			img: 'assets/images/mac-3.png',
+			title: 'Laptop Apple MacBook Pro 2018',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 4,
+			img: 'assets/images/mac-4.png',
+			title: 'Laptop APPLE MacBook Pro 2020',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 5,
+			img: 'assets/images/mac-1.png',
+			title: 'MacBook Air M1 256GB Silver',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 6,
+			img: 'assets/images/mac-2.png',
+			title: 'MacBook Air M1 256GB Black',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 7,
+			img: 'assets/images/mac-1.png',
+			title: 'MacBook Air 2017 128GB (MQD32SA/A)',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 8,
+			img: 'assets/images/mac-2.png',
+			title: 'MacBook Air M1 256GB Yellow',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 9,
+			img: 'assets/images/mac-3.png',
+			title: 'Laptop Apple MacBook Pro 2018',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 10,
+			img: 'assets/images/mac-4.png',
+			title: 'Laptop APPLE MacBook Pro 2020',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 11,
+			img: 'assets/images/mac-1.png',
+			title: 'MacBook Air M1 256GB Silver',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 12,
+			img: 'assets/images/mac-2.png',
+			title: 'MacBook Air M1 256GB Black',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		]
+	}
+});
+
+// Khoi tao doi tuong vue
+var vue = new Vue({
+	el: '.app3',
+	data: {
+		listNew3: [
+		{
+			id: 1,
+			img: 'assets/images/asus-1.png',
+			title: 'Asus ZenBook UX371EA-HL701TS',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 2,
+			img: 'assets/images/asus-2.png',
+			title: 'Laptop Asus Zenbook UX363EA',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 3,
+			img: 'assets/images/asus-3.png',
+			title: 'Laptop Asus Vivobook S533EQ',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 4,
+			img: 'assets/images/asus-4.png',
+			title: 'Laptop ASUS VivoBook S14',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 5,
+			img: 'assets/images/asus-5.png',
+			title: 'Laptop ASUS ZenBook UX430UA',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 6,
+			img: 'assets/images/asus-6.png',
+			title: 'Laptop ASUS ZenBook 13 UX331UN',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 7,
+			img: 'assets/images/asus-1.png',
+			title: 'Asus ZenBook UX371EA-HL701TS',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 8,
+			img: 'assets/images/asus-2.png',
+			title: 'Laptop Asus Zenbook UX363EA',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 9,
+			img: 'assets/images/asus-3.png',
+			title: 'Laptop Asus Vivobook S533EQ',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 10,
+			img: 'assets/images/asus-4.png',
+			title: 'Laptop ASUS VivoBook S14',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 11,
+			img: 'assets/images/asus-5.png',
+			title: 'Laptop ASUS ZenBook UX430UA',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 12,
+			img: 'assets/images/asus-6.png',
+			title: 'Laptop ASUS ZenBook 13 UX331UN',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		]
+	}
+});
+
+// Khoi tao doi tuong vue
+var vue = new Vue({
+	el: '.app4',
+	data: {
+		listNew4: [
+		{
+			id: 1,
+			img: 'assets/images/dell-1.png',
+			title: 'Laptop Dell Inspiron 7373-TI501OW',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 2,
+			img: 'assets/images/dell-2.png',
+			title: 'Laptop Dell Vostro 5370-V5370A',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 3,
+			img: 'assets/images/dell-3.png',
+			title: 'Laptop Dell Inspiron 5584-N5I5413W',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 4,
+			img: 'assets/images/dell-4.png',
+			title: 'Laptop Dell G5 15 5500',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 5,
+			img: 'assets/images/dell-5.png',
+			title: 'Laptop Dell Inspiron 7570-782P81',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 6,
+			img: 'assets/images/dell-1.png',
+			title: 'Laptop Dell Vostro 5570-GT537A',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 7,
+			img: 'assets/images/dell-1.png',
+			title: 'Laptop Dell Inspiron 7373-TI501OW',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 8,
+			img: 'assets/images/dell-2.png',
+			title: 'Laptop Dell Vostro 5370-V5370A',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 9,
+			img: 'assets/images/dell-3.png',
+			title: 'Laptop Dell Inspiron 5584-N5I5413W',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 10,
+			img: 'assets/images/dell-4.png',
+			title: 'Laptop Dell G5 15 5500',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 11,
+			img: 'assets/images/dell-5.png',
+			title: 'Laptop Dell Inspiron 7570-782P81',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 12,
+			img: 'assets/images/dell-1.png',
+			title: 'Laptop Dell Vostro 5570-GT537A',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		]
+	}
+});
+
+
+// Khoi tao doi tuong vue
+var vue = new Vue({
+	el: '.app5',
+	data: {
+		listNew5: [
+		{
+			id: 1,
+			img: 'assets/images/acer-1.png',
+			title: 'Acer Swift 3 SF315-52G-87N4',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 2,
+			img: 'assets/images/acer-2.png',
+			title: 'Laptop Acer Aspire E5-575-35M7',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 3,
+			img: 'assets/images/acer-3.png',
+			title: 'Laptop Acer Spin 5 SP513-52N-556V',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 4,
+			img: 'assets/images/acer-4.png',
+			title: 'Laptop Acer Swift 3 SF315-52',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 5,
+			img: 'assets/images/acer-5.png',
+			title: 'Laptop Acer Aspire 3 A315',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 6,
+			img: 'assets/images/acer-6.png',
+			title: 'Laptop Acer Swift 3 SF314',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 7,
+			img: 'assets/images/acer-1.png',
+			title: 'Acer Swift 3 SF315-52G-87N4',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 8,
+			img: 'assets/images/acer-2.png',
+			title: 'Laptop Acer Aspire E5-575-35M7',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 9,
+			img: 'assets/images/acer-3.png',
+			title: 'Laptop Acer Spin 5 SP513-52N-556V',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 10,
+			img: 'assets/images/acer-4.png',
+			title: 'Laptop Acer Swift 3 SF315-52',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 11,
+			img: 'assets/images/acer-5.png',
+			title: 'Laptop Acer Aspire 3 A315',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		{
+			id: 12,
+			img: 'assets/images/acer-6.png',
+			title: 'Laptop Acer Swift 3 SF314',
+			content: 'The internal 512 GB PCIe(R) NVMe(TM) M.2 SSD, and higher bandwidth, speed and efficiency with 16 GB DDR4-2666 SDRAM...'
+		},
+		]
+	}
+});
