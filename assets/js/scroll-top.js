3b99e4f270316b8d6f04a6ffdbe1494bad1c3828
NguyenTanPil <traila1008@gmail.com>
2021-01-28 17:41:12 +0700

    Add header to bottom

--- assets/js/scroll-top.js
+++ assets/js/scroll-top.js
@@ -5,6 +5,14 @@ window.addEventListener("scroll", () => {
     toTop.classList.add("active");
   } else {
     toTop.classList.remove("active");
-  }
+  } 
 })
 // end scroll top 
+const toBottom = document.querySelector(".nav-header");
+window.addEventListener("scroll", () => {
+	if(window.pageYOffset > 100) {
+		toBottom.classList.add("active");
+	} else {
+		toBottom.classList.remove("active");
+	}
+});
